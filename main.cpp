e0019781c1fbe9072f89ff5bb17ce3639090c94f
Angel Omaña <juan.omana.montanez@gmail.com>
2024-09-16 00:02:27 -0500

    Añado informe del proyecto, los cambios al programa se subiran mas tarde

--- main.cpp
+++ main.cpp
@@ -13,40 +13,41 @@ int botonInicio = 7;
 int botonDetener = 4;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(botonInicio, INPUT_PULLUP);
-    pinMode(botonDetener, INPUT_PULLUP);
-    lcd.begin(16, 2);
-    arr = new int[capacidad];
+  Serial.begin(9600);
+  pinMode(botonInicio, INPUT_PULLUP);
+  pinMode(botonDetener, INPUT_PULLUP);
+  lcd.begin(16, 2);
+  arr = new int[capacidad];
 }
 
 void loop() {
-    if (digitalRead(botonInicio) == HIGH) {
-        acquiringData = true;
-        lcd.clear();
-        lcd.print("Adq. datos");
-    }
+  if (digitalRead(botonInicio) == HIGH) {
+    acquiringData = true;
+    lcd.clear();
+    lcd.print("Adq. datos");
+  }
 
-    if (digitalRead(botonDetener) == HIGH) {
-        acquiringData = false;
-        lcd.clear();
-        lcd.print("Adq. detenida");
-        printArray();
-    }
+  if (digitalRead(botonDetener) == HIGH) {
+    acquiringData = false;
+    lcd.clear();
+    lcd.print("Adq. detenida");
+    printArray();
+  }
 
-    if (acquiringData) {
-        if (cantElementos == capacidad) {
-            redArr(arr, capacidad);
-        }
-        arr[cantElementos] = analogRead(analogPin);
-        //Serial.println(arr[cantElementos]);
-        cantElementos++;
-        //lcd.setCursor(0, 1);
-        //lcd.print("Valor: ");
-        //lcd.print(arr[cantElementos - 1]);
+  if (acquiringData) {
+    if (cantElementos == capacidad) {
+      redArr(arr, capacidad);
     }
-    delay(50);
+    arr[cantElementos] = analogRead(analogPin);
+    Serial.println(arr[cantElementos]);
+    cantElementos++;
+    delay(20);
+    //lcd.setCursor(0, 1);
+    //lcd.print("Valor: ");
+    //lcd.print(arr[cantElementos - 1]);
+  }
 }
+
 
 void redArr(int*& arr, int& capacidad) {
     int nuevaCap = capacidad * 2;
@@ -59,15 +60,15 @@ void redArr(int*& arr, int& capacidad) {
     capacidad = nuevaCap;
 }
 void printArray() {
-    Serial.println("Contenido del arreglo:");
-    for (int i = 0; i < cantElementos; i++) {
-        Serial.print("Indice ");
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.println(arr[i]);
-    }
+  Serial.println("Contenido del arreglo:");
+  for (int i = 0; i < cantElementos; i++) {
+    Serial.print("Indice ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(arr[i]);
+  }
 }
 
 void cleanup() {
-    delete[] arr;
+  delete[] arr;
 }
